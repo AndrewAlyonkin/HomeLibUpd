212e29a5fa7e7e3f6f014645db6f15592440fb6b
Andrew Alyonkin <oxqq@ya.ru>
2021-04-24 15:26:00 +0800

    Library updated

---
+++ src/main/java/edu/alenkin/HomeLibUpd/HelloApplication.java
@@ -0,0 +1,9 @@
+package edu.alenkin.HomeLibUpd;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
